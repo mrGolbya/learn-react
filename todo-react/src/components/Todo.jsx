b62aa9547e5907f38bb08d23da8a2e45578ab9bf
mrGolbya <mr.golbya@gmail.com>
2025-11-10 14:57:48 +0300

    feat: добавил пропсы для отображения инфо списка задач

--- todo-react/src/components/Todo.jsx
+++ todo-react/src/components/Todo.jsx
@@ -14,7 +14,10 @@ const Todo = () => {
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm/>
       <SearchTaskForm />
-      <TodoInfo />
+      <TodoInfo 
+        total={tasks.length}
+        done={tasks.filter(({isDone})=> isDone).length}
+      />
       <TodoList tasks={tasks} />
     </div>
   );
