ea73fc30c66f5f2f922df7baf437c1b87b048dfa
mrGolbya <mr.golbya@gmail.com>
2025-11-10 11:33:45 +0300

    feat: синтаксис в jsx

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,8 +1,16 @@
+const username = 'Alexander'
+
 const App = () => {
   return (
-    <div>
-      <h1>To Do List</h1>
-    </div>
+    <>
+      <h1 className="title">To Do List</h1>
+      <p style={{color: 'red'}}>Hi, {username}!</p>
+      <p>{new Date().toLocaleDateString()}</p>
+      {/*это комментарий JSX*/}
+      <hr />
+      <labal htmlFor='email' >Email:</labal>
+      <input id="email" type="email" required/>
+    </>
   )
 }
 
